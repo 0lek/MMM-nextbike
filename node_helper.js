4a54608476ca57f35a2a81f917d679bb69f6b8f0
0lek <github@aleksanderjarosz.pl>
2017-09-17 14:08:31 +0200

    Fixing...

--- node_helper.js
+++ node_helper.js
@@ -33,6 +33,7 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         if(notification === 'CONFIG'){
         	this.config = payload;
+        	var dummy = this.config.stationID;
 			var socialbike_url = this.config.apiBase + this.getParams();
 			this.getData(socialbike_url, this.config.stationID);
         }
